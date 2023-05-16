aaa774d6ad19c83f526e90380e909664978940ea
Álvaro Leva Ligero <levalvaro@gmail.com>
2023-05-16 14:07:44 +0200

    PMU component done. A function PMU_init initialize the PMU (EN = 1) and PMU_end stops de PMU (EN = 0).

---
+++ Core/Src/components/PMU.c
@@ -0,0 +1,27 @@
+/*
+ * PMU.c
+ *
+ *  Created on: 16 may. 2023
+ *      Author: Leva Álvaro
+ *      Definiremos las funciones de control de la PMU:
+ *      - PMU_init
+ *      - PMU_end
+ */
+
+#include "components/PMU.h"
+
+//Function to initialize the PMU ( EN = 1 )
+void PMU_init(void) {
+
+	HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_SET); // EN = 1
+
+}
+
+//Function to end the PMU ( EN = 0 )
+void PMU_end(void) {
+
+	HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_RESET); // EN = 0
+
+}
+
+
