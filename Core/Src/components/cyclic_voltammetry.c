fa40adbdf8ac9372b09e2cd59298a10f12087b42
Álvaro Leva Ligero <levalvaro@gmail.com>
2023-05-23 13:59:31 +0200

    CV source file created.

---
+++ Core/Src/components/cyclic_voltammetry.c
@@ -0,0 +1,19 @@
+/*
+ * cyclic_voltammetry.c
+ *
+ *  Created on: 23 may. 2023
+ *      Author: Alvaro Leva
+ */
+
+#include "components/stm32main.h"
+#include "components/cyclic_voltammetry.h"
+#include "components/masb_comm_s.h"
+#include "components/formulas.h"
+
+void make_CV(struct CV_configuration_S) {
+
+	//Fix the tension of the cell Vcell to eBegin
+
+
+
+}
